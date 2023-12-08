3d9377f322170ef8b640fec53859be9f53c56e5f
william <williamwei9707@gmail.com>
2023-12-08 14:22:34 +0800

    add scanline threshold selection method. but not that useful.

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ import os
 import numpy as np
 from sklearn.cluster import DBSCAN
 from scipy import optimize
+from scipy.interpolate import interp1d
+import glob
+import re
+from typing import List
 
 
-rootdir = "/home/william/extdisk/Data/cybathlon/cppresult"
-img_path = "000145_mask.jpg" # 145 or 127
+rootdir = "/home/william/extdisk/Data/cybathlon/cppresult/"
+img_path = "000127_mask.jpg" # 145 or 127
 index = img_path[:6]
 path_seg  = cv2.imread(os.path.join(rootdir, img_path))
 path_seg = cv2.GaussianBlur(path_seg, [3, 3], cv2.BORDER_DEFAULT)
@@ -41,6 +45,14 @@ coordinates = list(zip(valid_points[0], valid_points[1]))
 
 # print("tan(1) = ", np.tan(np.deg2rad(1)))
 
+def get_filenames(rootdir:str, specifier:str):
+    conditional_filenames = glob.glob(rootdir + f"*{specifier}*")
+    return conditional_filenames
+
+def make_skeleton(binary_image):
+    skeleton = skeletonize(binary_image, method="lee")
+    return skeleton
+
 def l2_norm(a, b):
     return np.linalg.norm(a-b)
 
@@ -48,7 +60,7 @@ def show_pic(img):
     plt.imshow(img)
     plt.show()
 
-def y_coord_transform(h:int, coordinates:list[np.array]):
+def y_coord_transform(h:int, coordinates:List[np.array]):
     """coordinate sample is (y, x), thus we
     change the coordinate system to (h-y, x)
     """
@@ -63,7 +75,7 @@ def y_coord_restore(h:int, transformed_coordinates:np.array):
     coord_restore[:, 0] = -coord_restore[:, 0] + h
     return coord_restore
 
-def diffusion_method(coordinates:list[np.array], delta_theta=1, begin_angle=0, end_angle=90):
+def diffusion_method(coordinates:List[np.array], delta_theta=1, begin_angle=0, end_angle=90):
     """this is a classification problem, which aims to classify
     the point is whether at left boundary or right. Therefore we
     construct a vector y=kx (k iterates by delta theta) to find the 
@@ -76,16 +88,88 @@ def diffusion_method(coordinates:list[np.array], delta_theta=1, begin_angle=0, e
             pass
 
 
-# blank_paper = np.zeros_like(grads_erode, dtype=np.uint8)
-# for coord in coordinates:
-#     blank_paper[coord[0], coord[1]] = 255
+def construct_map(coordinates):
+    """change the data-structure from list[np.array] to
+    key-value mapping type. For the convinience of searching
+    by key
+    """
+    coordinates_dict = {}
+    for coord in coordinates:
+        if coord[0] not in coordinates_dict:
+            # If not, initialize it with an empty list
+            coordinates_dict[coord[0]] = []
+        coordinates_dict[coord[0]].append(coord[1])
+    return coordinates_dict
 
-# plt.imshow(blank_paper)
-# plt.show()
-# tan_theta_valid_info = []
-# coord_and_grad = []
-# coord = []
+def scanline_threshold_selection(coordinates:List[np.array], tolerance=10):
+    coord_map = construct_map(coordinates)
+    x_distance = []
+    for k in coord_map.keys():
+        pixels = coord_map[k]
+        if len(pixels) > 1:
+            min_val = np.min(pixels)
+            max_val = np.max(pixels)
+            x_diff = max_val - min_val
+            x_distance.append((k, x_diff))
 
+    x_distance_array = np.array(x_distance)
+    # print(x_distance_array)
+
+    diff_median = np.median(x_distance_array[:, 1])
+    diff_mean = np.mean(x_distance_array[:, 1])
+    diff_max = np.max(x_distance_array[:, 1])
+    diff_min = np.min(x_distance_array[:, 1])
+
+    if np.abs(diff_median - diff_mean) > tolerance:
+        if np.abs(diff_median - diff_min) < np.abs(diff_median - diff_max):
+            selected = np.where(x_distance_array[:, 1] < diff_mean)
+        else:
+            selected = np.where(x_distance_array[:, 1] > diff_mean)
+        x_distance_array = x_distance_array[selected]
+    return x_distance_array
+
+
+
+def find_middle_lane_rowwise(mask):
+    if len(mask.shape) == 3:
+        h, w, _ = mask.shape
+    else:
+        h, w = mask.shape
+    middle_lane = np.zeros(h, dtype=np.int32)
+
+    for i in range(h):
+        row = mask[i, :]
+        white_pixels = np.where(row == 255)[0]
+        if white_pixels.size > 0:
+            middle_lane[i] = np.mean(white_pixels).astype(np.int32)
+    
+    return middle_lane
+
+
+def calculate_middle_lane(mask, coordinates, tolerance=10):
+    selection = scanline_threshold_selection(coordinates, tolerance)
+    rough_middle_lane = find_middle_lane_rowwise(mask)
+    rough_middle_lane = np.array(rough_middle_lane)
+    optimized_middle_lane = []
+    for i in range(len(selection)):
+        optimized_middle_lane.append((selection[i][0], rough_middle_lane[selection[i][0]]))
+    return optimized_middle_lane
+
+
+def find_middle_lane_colwise(mask):
+    if len(mask.shape) == 3:
+        h, w, _ = mask.shape
+    else:
+        h, w = mask.shape
+    middle_lane = np.zeros(w, dtype=np.int32)
+
+    for i in range(w):
+        col = mask[:, i]
+        white_pixels = np.where(col == 255)[0]
+        if white_pixels.size > 0:
+            middle_lane[i] = np.mean(white_pixels).astype(np.int32)
+    
+    return middle_lane
 
 
 # cur_idx = 0
@@ -111,20 +195,6 @@ def diffusion_method(coordinates:list[np.array], delta_theta=1, begin_angle=0, e
 
 # print(tan_theta_valid_info)
             
-        
-
-
-
-# fig, axs = plt.subplots(2, 2)
-# axs[0, 0].imshow(grads)
-# axs[0, 0].axis('off')
-# axs[0, 1].imshow(grads_erode)
-# axs[0, 1].axis('off')
-# axs[1, 0].imshow(gy_down_shift)
-# axs[1, 0].axis('off')
-# axs[1, 1].imshow(gy_up_shift)
-# axs[1, 1].axis('off')
-# plt.show()
 
 def apply_dbscan_to_column(column, eps=2, min_samples=2):
     """ Apply DBSCAN clustering to a single column """
@@ -163,43 +233,6 @@ def find_middle_lane_dbscan(binary_mask, eps=2, min_samples=2):
 
     return middle_lane
 
-def find_middle_lane_rowwise(mask):
-    h, w = mask.shape
-    middle_lane = np.zeros(h, dtype=np.int32)
-
-    for i in range(h):
-        row = mask[i, :]
-        white_pixels = np.where(row == 255)[0]
-        if white_pixels.size > 0:
-            middle_lane[i] = np.mean(white_pixels).astype(np.int32)
-    
-    return middle_lane
-
-def find_middle_lane_colwise(mask):
-    h, w = mask.shape
-    middle_lane = np.zeros(w, dtype=np.int32)
-
-    for i in range(w):
-        col = mask[:, i]
-        white_pixels = np.where(col == 255)[0]
-        if white_pixels.size > 0:
-            middle_lane[i] = np.mean(white_pixels).astype(np.int32)
-    
-    return middle_lane
-
-
-def construct_map(coordinates):
-    """change the data-structure from list[np.array] to
-    key-value mapping type. For the convinience of searching
-    by key
-    """
-    coordinates_dict = {}
-    for coord in coordinates:
-        if coord[0] not in coordinates_dict:
-            # If not, initialize it with an empty list
-            coordinates_dict[coord[0]] = []
-        coordinates_dict[coord[0]].append(coord[1])
-    return coordinates_dict
 
 
 def fit_polynomials_scalar(x, coef):
@@ -219,13 +252,12 @@ def cost_func(domain, target_coeffs, f_coeffs, g_coeffs):
 
     return np.sum((target - f)**2 + (target - g)**2)
 
-def rotation_method():
-    middle_lane = find_middle_lane_rowwise(path_seg[:,:,0])
+def rotation_method(path_seg:np.array):
+    middle_lane = find_middle_lane_rowwise(path_seg)
     middle_lane_coords = []
     for i in range(len(middle_lane)):
         if middle_lane[i] != 0:
             middle_lane_coords.append((i, middle_lane[i]))
-
 
     seg_copy = np.copy(path_seg)
     for y, x in middle_lane_coords:
@@ -233,78 +265,104 @@ def rotation_method():
 
     # fit polynomial functions
     middle_lane_coords = np.array(middle_lane_coords)
-    func_middle_lane_coef = np.polyfit(middle_lane_coords[:, 0], middle_lane_coords[:, 1], 3)
+    func_middle_lane_coef = np.polyfit(middle_lane_coords[:, 1], middle_lane_coords[:, 0], 5)
 
     for x in range(W):
         y = int(fit_polynomials_scalar(x, func_middle_lane_coef))
-        cv2.circle(seg_copy, (y, x), 1, (0, 0, 255), 2)
+        cv2.circle(seg_copy, (x, y), 1, (0, 0, 255), 2)
 
     path_seg_t = cv2.rotate(path_seg, cv2.ROTATE_90_COUNTERCLOCKWISE)
 
-    middle_lane_t = find_middle_lane_rowwise(path_seg_t[:,:,0])
+    middle_lane_t = find_middle_lane_rowwise(path_seg_t)
     middle_lane_coords_t = []
     for i in range(len(middle_lane_t)):
         if middle_lane_t[i] != 0:
             middle_lane_coords_t.append((i, middle_lane_t[i]))
 
     middle_lane_coords_t = np.array(middle_lane_coords_t)
-    middle_lane_coords_t_restore = np.array([[0, -1], [1, 0]]) @ middle_lane_coords_t.T + np.array([[W], [0]])
-    # tmp = np.array([[0, -1], [1, 0]])
-    # print(tmp.shape)
-    # print(middle_lane_coords_t.shape)
-    for y, x in middle_lane_coords_t:
-        cv2.circle(seg_copy, (np.abs(y-W), x), 1, (255, 0, 0), 2)
+    # middle_lane_coords_t_restore = np.array([[0, -1], [1, 0]]) @ middle_lane_coords_t.T + np.array([[W], [0]])
+    middle_lane_coords_t_restore = np.copy(middle_lane_coords_t)
+    middle_lane_coords_t_restore[:, 1] = W - middle_lane_coords_t[:, 0]
+    middle_lane_coords_t_restore[:, 0] = middle_lane_coords_t[:, 1]
 
-    # middle_lane_coords_t = np.array(middle_lane_coords_t)
-    # middle_lane_coords_t_restore = np.array(middle_lane_coords_t)
-    # middle_lane_coords_t_restore[:, 1] = middle_lane_coords_t[:, 1]
-    # middle_lane_coords_t_restore[:, 0] = W - middle_lane_coords_t_restore[:, 0]
+    # for y, x in middle_lane_coords_t:
+    #     cv2.circle(seg_copy, (np.abs(y-W), x), 1, (255, 0, 0), 2)
+
+    for y, x in middle_lane_coords_t_restore:
+        cv2.circle(seg_copy, (x, y), 1, (255, 0, 0), 2)
 
     # for coord in middle_lane_coords_t_restore:
     #     cv2.circle(seg_copy, (coord[0], coord[1]), 1, (128, 128, 128), 2)
 
-    middle_lane_coords_t = np.array(middle_lane_coords_t)
+    # middle_lane_coords_t = np.array(middle_lane_coords_t)
     # func_middle_lane_t_coef = np.polyfit(middle_lane_coords_t_restore[:, 1], middle_lane_coords_t_restore[:, 0], 3)
-    func_middle_lane_t_coef = np.polyfit(W - middle_lane_coords_t_restore[:, 0], middle_lane_coords_t_restore[:, 1], 3)
+    func_middle_lane_t_coef = np.polyfit(middle_lane_coords_t_restore[:, 1], middle_lane_coords_t_restore[:, 0], 5)
 
     for x in range(W):
         y = int(fit_polynomials_scalar(x, func_middle_lane_t_coef))
-        cv2.circle(seg_copy, (y, x), 1, (128, 128, 128), 2)
+        cv2.circle(seg_copy, (x, y), 1, (128, 128, 128), 2)
 
     # # restore to the same coordinate system
     
     min_1_x = np.min(middle_lane_coords[:, 1])
     max_1_x = np.max(middle_lane_coords[:, 1])
-    # # min_1_y = np.min(middle_lane_coords[:, 0])
-    # # max_1_y = np.max(middle_lane_coords[:, 0])
+
+    min_2_x = np.min(middle_lane_coords_t_restore[:, 1])
+    max_2_x = np.max(middle_lane_coords_t_restore[:, 1])
+
+    domain_x = range(max(min_1_x, min_2_x), max(max_1_x, max_2_x))
+    print(min_1_x, max_1_x, min_2_x, max_2_x)
+    # initial_guess = [1, 1, 1, 1]
     
-    # # # vertical_min = np.min(np.abs(middle_lane_coords_t[:, 0]-W))
-    # # # vertical_max = np.max(np.abs(middle_lane_coords_t[:, 0]-W))
+    # ideal_curve = optimize.minimize(lambda x: cost_func(domain_x, x, func_middle_lane_coef, func_middle_lane_t_coef), initial_guess)
+    print(func_middle_lane_coef)
+    print(func_middle_lane_t_coef)
+    # print(ideal_curve.x)
 
-    min_2_x = np.min(middle_lane_coords_t_restore[:, 0])
-    max_2_x = np.max(middle_lane_coords_t_restore[:, 0])
-    # print(min_1_x, max_1_x, min_2_x, max_2_x)
-    # # min_2_y = np.min(middle_lane_coords[:, 1])
-    # # max_2_y = np.max(middle_lane_coords[:, 1])
+    # for x in range(W):
+    #     y = int(fit_polynomials_scalar(x, ideal_curve.x))
+    #     cv2.circle(seg_copy, (x, y), 1, (255, 192, 203), 2)
+    poly1 = np.poly1d(func_middle_lane_coef)
+    poly2 = np.poly1d(func_middle_lane_t_coef)
 
-    domain_x = range(max(min_1_x, min_2_x), min(max_1_x, max_2_x))
-    # # domain_y = range(max(min_1_y, min_2_y), min(max_1_y, max_2_y))
-    initial_guess = [1, 1, 1, 1]
-    
-    ideal_curve = optimize.minimize(lambda x: cost_func(domain_x, x, func_middle_lane_coef, func_middle_lane_t_coef), initial_guess)
-    print(ideal_curve.x)
+    y1 = poly1(domain_x)
+    y2 = poly2(domain_x)
 
-    for x in range(W):
-        y = int(fit_polynomials_scalar(x, ideal_curve.x))
-        cv2.circle(seg_copy, (y, x), 1, (255, 192, 203), 2)
+    y_avg = (y1 + y2) / 2
+
+    ideal_curve = interp1d(domain_x, y_avg, kind="cubic")
+    for x in domain_x:
+        y = int(ideal_curve(x))
+        cv2.circle(seg_copy, (x, y), 1, (255, 192, 203), 2)
 
     return seg_copy
 
 
 if __name__ == "__main__":
-    res = rotation_method()
-    show_pic(res)
-    # cv2.imwrite(f"debug_full_vis.png", res[:,:,::-1])
+    # res = rotation_method(path_seg)
+    # show_pic(res)
+    # optimized_middle_lane = calculate_middle_lane(path_seg, coordinates)
+    # seg_copy = np.copy(path_seg)
+    # for y, x in optimized_middle_lane:
+    #     cv2.circle(seg_copy, (x, y), 1, (255, 192, 203), 2)
+    # show_pic(seg_copy)
+    all_filenames = get_filenames(rootdir, "mask")
+    sourcedir = "/home/william/extdisk/Data/cybathlon" 
+    outdir = os.path.join(sourcedir, "debug_middle_path")
+    os.makedirs(outdir, exist_ok=True)
+    for path in all_filenames:
+        path_seg = cv2.imread(path)
+        path_seg = cv2.GaussianBlur(path_seg, [3, 3], cv2.BORDER_DEFAULT)
+        optimized_middle_lane = calculate_middle_lane(path_seg, coordinates)
+        seg_copy = np.copy(path_seg)
+        for y, x in optimized_middle_lane:
+            cv2.circle(seg_copy, (x, y), 1, (255, 192, 203), 2)
+        match = re.search(r'(\d+)_mask\.jpg', path)
+        cv2.imwrite(os.path.join(outdir, f"{match.group(1)}_center_line.jpg"), seg_copy[:,:,::-1])
+    
+    print("done!")
+
+        
     import gc
     gc.collect()
 
